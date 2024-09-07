ae055620a2c7ba5884d5e9ee0e497ccbfb70d7de
Vincenzo Bonatti <133982612+VincenzoBonatti@users.noreply.github.com>
2024-09-07 00:17:47 +0000

    excluir crud

--- my-app/app/Componentes/Adaptadores/CadastroProduto/index.tsx
+++ my-app/app/Componentes/Adaptadores/CadastroProduto/index.tsx
@@ -1,9 +1,12 @@
+import Produto from "@/app/Models/produto"
+import axios from "axios"
 import { useState } from "react"
-import { Text, TextInput, View } from "react-native"
+import { Button, Text, TextInput, View } from "react-native"
 
 const CadastroProduto = () => {
     let [nome,setNome] = useState("")
     let [descricao,setDescricao] = useState("")
+    let [preco,setPreco] = useState(0)
 
     return(
         <View>
@@ -12,8 +15,27 @@ const CadastroProduto = () => {
             
             <Text  >Descrição:</Text>
             <TextInput onChangeText={setDescricao} value={descricao}></TextInput>
+
+            <Text  >Preço:</Text>
+            <TextInput onChangeText={(v) => 
+                setPreco( isNaN(parseFloat(v))? 0: parseFloat(v))} 
+                
+                inputMode="numeric" value={preco.toString()}></TextInput>
+        
+            <Button title="Cadastrar" onPress={cadastrar} />
+        
         </View>
     )
+
+    function cadastrar() {
+        let produto: Produto = {
+            nome: nome,
+            descricao: descricao,
+            preco: preco
+        } as Produto;
+        let api = "https://api-docker-2t8m.onrender.com/api/produtos"
+        axios.post(api,produto)
+    }
 }
 
 export default CadastroProduto
--- my-app/app/Componentes/Adaptadores/ItemProduto/index.tsx
+++ my-app/app/Componentes/Adaptadores/ItemProduto/index.tsx
@@ -1,6 +1,7 @@
 import Style from "@/app/Styles/Default";
-import { Image, Text, View } from "react-native";
+import { Alert, Button, Image, Text, View } from "react-native";
 import Produto from "@/app/Models/produto";
+import axios from "axios";
 
 
 interface PropProd{
@@ -9,11 +10,22 @@ interface PropProd{
 
 
 const ItemProduto:React.FC<PropProd> = ( {produto})=> {
+    
+    function Excluir(id: number) {
+        let api = "https://api-docker-2t8m.onrender.com/api/produtos"
+        axios.delete(`${api}/${id}`)
+        .then((resp) =>{
+            Alert.alert("Produto excluido com sucesso")
+            alert("produto excluido com sucesso")
+        })
+    }
+
     return(
         <View style={Style.card}>
             <Text style={Style.cardText}>{produto.nome}</Text>
             <Text style={Style.cardText}>R$ {produto.preco}</Text>
             <Image source={{uri:produto.foto}} style={Style.image}/>
+            <Button title="excluir" onPress={() => {Excluir(produto.id)}} />
         </View>
     )
 }
